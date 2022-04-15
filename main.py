8bf445ca6810d2abe2c1e33af358499009d67e60
Matías Mendieta <mendieta@knights.ucf.edu>
2022-04-14 22:23:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def set_random_seed(seed=1):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    ## NOTE: If you want every run to be exacatly the same each time
+    ## NOTE: If you want every run to be exactly the same each time
     ##       uncomment the following lines
     # torch.backends.cudnn.deterministic = True
     # torch.backends.cudnn.benchmark = False
