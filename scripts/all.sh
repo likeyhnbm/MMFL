f909100ccdfbba298ec5cb38b6cfd5e0e99632f2
Guangyu Sun <guangyu@knights.ucf.edu>
2022-09-09 02:51:00 -0400

    Update: requirements & scripts

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,13 +1,13 @@
 # Standard setting
 # sh scripts/c16_r_1_alpha_0.5/prompt.sh
-sh scripts/c16_r_1_alpha_0.5/pretrain.sh
+# sh scripts/c16_r_1_alpha_0.5/pretrain.sh
 # More client Do it later
 # sh scripts/c64_r_1_alpha_0.5/prompt.sh
 # sh scripts/c64_r_1_alpha_0.5/pretrain.sh
 
 # Sampling
 # sh scripts/c64_r_0.25_alpha_0.5/prompt.sh
-sh scripts/c64_r_0.25_alpha_0.5/pretrain.sh
+# sh scripts/c64_r_0.25_alpha_0.5/pretrain.sh
 
 # Sampling less
 # sh scripts/c64_r_0.125_alpha_0.5/prompt.sh
@@ -16,7 +16,7 @@ sh scripts/c64_r_0.25_alpha_0.5/pretrain.sh
 # Reduce alpha
 # Standard setting
 # sh scripts/c16_r_1_alpha_0.1/prompt.sh
-sh scripts/c16_r_1_alpha_0.1/pretrain.sh
+# sh scripts/c16_r_1_alpha_0.1/pretrain.sh
 # More client Do it later
 # sh scripts/c64_r_1_alpha_0.1/prompt.sh
 # sh scripts/c64_r_1_alpha_0.1/pretrain.sh
