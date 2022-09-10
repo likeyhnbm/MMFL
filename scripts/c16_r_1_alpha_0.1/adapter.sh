b4bc9e4bbb00b3994f97bece22295889f37f2ae3
Guangyu Sun <guangyu@knights.ucf.edu>
2022-09-10 01:10:24 -0400

    Update: More general scripts

---
+++ scripts/c16_r_1_alpha_0.1/adapter.sh
@@ -0,0 +1,4 @@
+for lr in 1e-4 5e-4 1e-3 5e-3 1e-2
+do
+        python main.py --partition_alpha 0.1 --method "adapter" --client_sample 0.125 --client_number 64 --thread_number 8 --comm_round 50 --lr $lr --epochs 10 --data_dir dataset/cifar100/ --batch_size 64 --vpt_type Deep --vit_type vit_base_patch16_224_in21k --optimizer sgd --sam_mode none --vpt_projection -1
+done
