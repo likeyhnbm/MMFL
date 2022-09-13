bd0d7e0c11796a331f64ba889cbf50ec4ac6f059
Guangyu Sun <guangyu@knights.ucf.edu>
2022-09-12 22:28:26 -0400

    Add: DPSGD

--- methods/scratch.py
+++ methods/scratch.py
@@ -2,6 +2,7 @@ import torch
 import wandb
 import logging
 from methods.base import Base_Client, Base_Server
+from data_preprocessing.dpsgd import DPSGD
 
 class Client(Base_Client):
     def __init__(self, client_dict, args):
@@ -10,10 +11,15 @@ class Client(Base_Client):
         self.model = self.model_type(client_dict['type'], num_classes=self.num_classes, pretrained=False).to(self.device)
         self.criterion = torch.nn.CrossEntropyLoss().to(self.device)
 
-        if args.optimizer == 'sgd':
-                self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.args.lr, momentum=0.9, weight_decay=self.args.wd, nesterov=True)
-        elif args.optimizer == 'adamw':
-            self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.args.lr, weight_decay=self.args.wd)
+        params = filter(lambda p: p.requires_grad,self.model.parameters())
+
+        if args.dp:
+            self.optimizer = DPSGD(params, lr=self.args.lr, momentum=0.9, weight_decay=self.args.wd, nesterov=True)
+        else:
+            if args.optimizer == 'sgd':
+                    self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.args.lr, momentum=0.9, weight_decay=self.args.wd, nesterov=True)
+            elif args.optimizer == 'adamw':
+                self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.args.lr, weight_decay=self.args.wd)
 
 class Server(Base_Server):
     def __init__(self,server_dict, args):
