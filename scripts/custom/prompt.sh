2d0c439a60ba142fecb36bf4c312626f583e1a40
Guangyu Sun <guangyu@knights.ucf.edu>
2022-09-12 21:37:15 -0400

    Modify: scripts

--- scripts/custom/prompt.sh
+++ scripts/custom/prompt.sh
@@ -4,8 +4,9 @@ client_sample=$2
 partition=$3
 alpha=$4
 thread_number=$5
+data_dir=$6
 
-for lr in 5e-3 1e-2
+for lr in 1e-2
 do
-        python main.py --partition_alpha $alpha --method "prompt_official" --client_sample $client_sample --client_number $client_num --thread_number $thread_number --comm_round 50 --lr $lr --epochs 10 --data_dir dataset/cifar100/ --batch_size 64 --vpt_type Deep --vit_type vit_base_patch16_224_in21k --optimizer sgd --sam_mode none --vpt_projection -1 --partition_method $partition
+        python main.py --partition_alpha $alpha --method "prompt_official" --client_sample $client_sample --client_number $client_num --thread_number $thread_number --comm_round 50 --lr $lr --epochs 10 --data_dir $data_dir --batch_size 64 --vpt_type Deep --vit_type vit_base_patch16_224_in21k --optimizer sgd --sam_mode none --vpt_projection -1 --partition_method $partition
 done
---
+++ scripts/medical.sh
@@ -0,0 +1,2 @@
+# client_num client_sample partition_method alpha thread_num
+sh scripts/custom/prompt.sh 64 0.125 hetero 0.1 8 dataset/chestxray/
