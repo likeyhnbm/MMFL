c800a454e5dffd11bbc9e0f2aea257099c9b50b5
Guangyu Sun <guangyu@knights.ucf.edu>
2022-09-14 01:41:54 -0400

    Add: scripts for scratch

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,5 +1,10 @@
 # client_num client_sample partition_method alpha thread_num dataset dp
-sh scripts/custom/adapter.sh 64 0.125 homo 0.5 8 dataset/cifar100/
-sh scripts/custom/bias.sh 64 0.125 homo 0.5 8 dataset/cifar100/
-sh scripts/custom/head.sh 64 0.125 homo 0.5 8 dataset/cifar100/
-sh scripts/custom/prompt.sh 64 0.125 homo 0.5 8 dataset/cifar100/
+sh scripts/custom/bias.sh 16 1 hetero 0.1 16 dataset/cifar100/
+sh scripts/custom/head.sh 16 1 hetero 0.1 16 dataset/cifar100/
+
+sh scripts/custom/scratch.sh 16 1 homo 0.1 16 dataset/cifar100/
+sh scripts/custom/head.sh 16 1 homo 0.1 16 dataset/cifar100/
+sh scripts/custom/pretrain.sh 16 1 homo 0.1 16 dataset/cifar100/
+sh scripts/custom/prompt.sh 16 1 homo 0.1 16 dataset/cifar100/
+sh scripts/custom/adapter.sh 16 1 homo 0.1 16 dataset/cifar100/
+sh scripts/custom/bias.sh 16 1 homo 0.1 16 dataset/cifar100/
