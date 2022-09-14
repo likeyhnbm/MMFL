2e56045d51469cd0e9a3cbc56e686fd992015827
Guangyu Sun <guangyu@knights.ucf.edu>
2022-09-14 01:21:31 -0400

    Update scripts

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,7 +1,5 @@
-# client_num client_sample partition_method alpha thread_num
-sh scripts/custom/adapter 16 1 hetero 0.5 8
-sh scripts/custom/adapter 16 1 hetero 0.1 8
-sh scripts/custom/adapter 64 0.125 hetero 0.5 8
-sh scripts/custom/adapter 64 0.125 hetero 0.1 8
-sh scripts/custom/adapter 16 1 homo 0.5 8
-sh scripts/custom/adapter 64 0.125 homo 0.5 8
+# client_num client_sample partition_method alpha thread_num dataset dp
+sh scripts/custom/adapter.sh 64 0.125 homo 0.5 8 dataset/cifar100/
+sh scripts/custom/bias.sh 64 0.125 homo 0.5 8 dataset/cifar100/
+sh scripts/custom/head.sh 64 0.125 homo 0.5 8 dataset/cifar100/
+sh scripts/custom/prompt.sh 64 0.125 homo 0.5 8 dataset/cifar100/
