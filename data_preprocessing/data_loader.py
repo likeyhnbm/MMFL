418c2be2eb35b3d97a4b5e21aab8d2e094fad626
Guangyu Sun <guangyu@knights.ucf.edu>
2022-09-19 00:40:28 -0400

    Remove: no finding from chestx

--- data_preprocessing/data_loader.py
+++ data_preprocessing/data_loader.py
@@ -227,8 +227,8 @@ def _data_transforms_cheestxray(datadir, img_size=1024):
     train_mean = [0.4976, 0.4976, 0.4976]
     train_std = [0.2496, 0.2496, 0.2496]
 
-    test_mean = [0.4724, 0.4724, 0.4724]
-    test_std = [0.2370, 0.2370, 0.2370]
+    test_mean = [0.4715, 0.4715, 0.4715]
+    test_std = [0.2406, 0.2406, 0.2406]
 
     train_transform = transforms.Compose([
         # transforms.ToPILImage(), # Must convert to PIL image for subsequent operations to run
