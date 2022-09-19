4cbe5d88b7df9092b845f6ea16303331986ba00d
Guangyu Sun <guangyu@knights.ucf.edu>
2022-09-19 00:59:34 -0400

    Update: scripts for chest

--- scripts/medical.sh
+++ scripts/medical.sh
@@ -1,3 +1,7 @@
 # client_num client_sample partition_method alpha thread_num
 # sh scripts/custom/prompt.sh 64 0.125 homo 0.1 8 dataset/chestxray/
-sh scripts/custom/pretrain.sh 64 0.125 homo 0.1 4 dataset/chestxray/
+sh scripts/chest/prompt.sh 16 1 homo 0.1 8 dataset/chestxray/
+sh scripts/chest/head.sh 16 1 homo 0.1 8 dataset/chestxray/
+sh scripts/chest/adapter.sh 16 1 homo 0.1 8 dataset/chestxray/
+# sh scripts/chest/bias.sh 16 1 homo 0.1 8 dataset/chestxray/
+# sh scripts/chest/pretrain.sh 16 1 homo 0.1 4 dataset/chestxray/
