958ef1c71ca769a5bb115e4082014bef993fe076
Guangyu Sun <guangyu@knights.ucf.edu>
2022-09-24 18:57:04 -0400

    Modify: scripts for adapter lr

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,6 +1,3 @@
 # client_num client_sample partition_method alpha thread_num dataset dp
-sh scripts/custom/pretrain.sh 16 0.125 homo 0.1 2 dataset/cifar100/ 
-sh scripts/custom/prompt.sh 16 0.125 homo 0.1 2 dataset/cifar100/
-sh scripts/custom/adapter.sh 16 0.125 homo 0.1 2 dataset/cifar100/ 
-sh scripts/custom/bias.sh 16 0.125 homo 0.1 2 dataset/cifar100/ 
-sh scripts/custom/head.sh 16 0.125 homo 0.1 2 dataset/cifar100/
+sh scripts/custom/adapter.sh 64 0.125 hetero 0.5 8 dataset/pcam/ --sample_num=20000
+
