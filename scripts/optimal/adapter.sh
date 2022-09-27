3ef224faf32f7637a08fcf6fd42f87400f3a8699
Guangyu Sun <guangyu@knights.ucf.edu>
2022-09-27 11:36:29 -0400

    Remove: local epochs and rounds in scripts

--- scripts/optimal/adapter.sh
+++ scripts/optimal/adapter.sh
@@ -9,5 +9,5 @@ other=$7
 
 for lr in 5e-3
 do
-        python main.py --partition_alpha $alpha --method "adapter" --client_sample $client_sample --client_number $client_num --thread_number $thread_number --comm_round 50 --lr $lr --epochs 10 --data_dir $data_dir --batch_size 64 --vpt_type Deep --vit_type vit_base_patch16_224_in21k --optimizer sgd --sam_mode none --vpt_projection -1 --partition_method $partition $other
+        python main.py --partition_alpha $alpha --method "adapter" --client_sample $client_sample --client_number $client_num --thread_number $thread_number --lr $lr --data_dir $data_dir --batch_size 64 --vpt_type Deep --vit_type vit_base_patch16_224_in21k --optimizer sgd --sam_mode none --vpt_projection -1 --partition_method $partition $other
 done
