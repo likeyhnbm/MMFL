30279a86b1f61c5f870b71a7926c53709177f60a
Guangyu Sun <guangyu@knights.ucf.edu>
2022-09-26 23:20:20 -0400

    Remove:all.sh

--- scripts/all.sh
+++ scripts/all.sh
@@ -8,10 +8,10 @@
 # # sh scripts/chest/prompt.sh 64 0.125 homo 0.1 8 dataset/chestxray/ 
 
 # hetero
-sh scripts/dp/pretrain.sh 16 0.125 hetero 0.1 2 dataset/cifar100/
-sh scripts/dp/prompt.sh 16 0.125 hetero 0.1 2 dataset/cifar100/
-sh scripts/dp/adapter.sh 16 0.125 hetero 0.1 2 dataset/cifar100/ 
-sh scripts/dp/bias.sh 16 0.125 hetero 0.1 2 dataset/cifar100/ 
-sh scripts/dp/head.sh 16 0.125 hetero 0.1 2 dataset/cifar100/
+# sh scripts/dp/pretrain.sh 16 0.125 hetero 0.1 2 dataset/cifar100/
+# sh scripts/dp/prompt.sh 16 0.125 hetero 0.1 2 dataset/cifar100/
+# sh scripts/dp/adapter.sh 16 0.125 hetero 0.1 2 dataset/cifar100/ 
+# sh scripts/dp/bias.sh 16 0.125 hetero 0.1 2 dataset/cifar100/ 
+# sh scripts/dp/head.sh 16 0.125 hetero 0.1 2 dataset/cifar100/
 
 
