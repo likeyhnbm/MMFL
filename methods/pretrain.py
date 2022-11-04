e53da685cd13d151fb1f4a02c4775df2e7fa060a
Guangyu Sun <guangyu@knights.ucf.edu>
2022-11-04 06:35:24 -0400

    Fix: server weights

--- methods/pretrain.py
+++ methods/pretrain.py
@@ -33,3 +33,15 @@ class Server(Base_Server):
     def __init__(self,server_dict, args):
         super().__init__(server_dict, args)
         self.model = self.model_type(server_dict['type'], num_classes=self.num_classes, pretrained=True).to(self.device)
+        if 'mae' in args.ssl:
+            dict = torch.load(args.ssl)
+            self.model.load_state_dict(dict['model'], strict=False)
+        elif 'moco' in args.ssl:
+            dict = torch.load(args.ssl)
+            # dict = { k[7:]:v for k,v in dict['state_dict'].items()}
+            new_dict = {}
+            for k,v in dict['state_dict'].items():
+                if 'head' not in k:
+                    new_dict.update({k[7:]:v})
+
+            self.model.load_state_dict(new_dict, strict=False)
