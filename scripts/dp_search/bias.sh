8e6672e62c6edda8cb41260f1d584f2c9fce3251
Guangyu Sun <guangyu@knights.ucf.edu>
2022-11-18 18:57:42 -0500

    Modify: scripts

--- scripts/dp_search/bias.sh
+++ scripts/dp_search/bias.sh
@@ -7,7 +7,7 @@ thread_number=$5
 data_dir=$6
 other=$7
 
-for lr in 5e-4 1e-4
+for lr in 3e-3 8e-4
 do
         python main.py --partition_alpha $alpha --method "bias" --client_sample $client_sample --client_number $client_num --thread_number $thread_number --lr $lr --data_dir $data_dir --batch_size 64 --vpt_type Deep --vit_type vit_base_patch16_224_in21k --optimizer sgd --sam_mode none --vpt_projection -1 --partition_method $partition $other
 done
